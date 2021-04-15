53c9c67d675e5f23f2c3a2e93aba431994ae720d
Michael Smith <mike@cadmia.io>
2021-04-15 23:31:11 +0100

    Use a custom http.Server with new errorLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,10 +21,15 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
